6b17fa591ca23da6443e5daa5a147d33dcb8550a
yoshi0202 <kobayashi020202@gmail.com>
2020-09-18 13:47:37 +0900

    feat: WIP add routing

--- app/src/main.go
+++ app/src/main.go
@@ -1,49 +1,76 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yoshi0202/grass-app/app/src/constant"
 	"github.com/yoshi0202/grass-app/app/src/services"
+	"github.com/yoshi0202/grass-app/app/src/services/github"
 )
-
-type GithubGrass struct {
-	Count string `json:"count"`
-	Date  string `json:"date"`
-}
-
-type GithubGrasses []GithubGrass
 
 func main() {
 	router := gin.Default()
 
+	// root
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "root page")
+	})
+
+	router.GET("/login", func(c *gin.Context) {
+		c.String(http.StatusOK, "login page")
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		c.String(http.StatusOK, "create session")
+	})
+
+	router.DELETE("/logout", func(c *gin.Context) {
+		c.String(http.StatusOK, "logout")
+	})
+
+	router.GET("/signup", func(c *gin.Context) {
+		c.String(http.StatusOK, "signup page")
+	})
+
+	router.POST("/signup", func(c *gin.Context) {
+		c.String(http.StatusOK, "signup")
+	})
+
+	// router.GET("/user/:id", func(c *gin.Context) {
+	// 	c.String(http.StatusOK, "user id : "+string(c.Param("id")))
+	// })
+
+	router.GET("/timeline", func(c *gin.Context) {
+		c.String(http.StatusOK, "timeline page")
+	})
+
+	router.GET("/regist", func(c *gin.Context) {
+		c.String(http.StatusOK, "enter github id pages")
+	})
+
+	router.POST("/regist", func(c *gin.Context) {
+		c.String(http.StatusOK, "regist user")
+	})
+
 	// This handler will match /user/john but will not match /user/ or /user
 	router.GET("/user/:name", func(c *gin.Context) {
-		apiRes := services.Get(c.Param("name"))
-		svgTags := services.FindSvgTag(`<svg(?: [\s\S]+?)?>[\s\S]*?<\/svg>`, apiRes)
-		rectArray := services.FindAllRectTag(`<rect.*?\/>`, svgTags)
-		gl := GithubGrasses{}
-		for _, v := range rectArray {
-			g := GithubGrass{}
-			g.Count = strings.Split(strings.Split(string(v), " ")[7], "\"")[1]
-			g.Date = strings.Split(strings.Split(string(v), " ")[8], "\"")[1]
-			gl = append(gl, g)
-		}
-		result, _ := json.Marshal(gl)
-
-		c.String(http.StatusOK, string(result))
+		con := constant.NewConst()
+		apiRes := github.GetGrass(c.Param("name"))
+		svgTags := services.FindSvgTag(con.SvgTag, apiRes)
+		rectArray := services.FindAllRectTag(con.RectTag, svgTags)
+		grasses := github.CreateGrasses(github.GithubGrasses{}, rectArray)
+		c.String(http.StatusOK, grasses)
 	})
 
 	// However, this one will match /user/john/ and also /user/john/send
 	// If no other routers match /user/john, it will redirect to /user/john/
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
-	})
+	// router.GET("/user/:name/*action", func(c *gin.Context) {
+	// 	name := c.Param("name")
+	// 	action := c.Param("action")
+	// 	message := name + " is " + action
+	// 	c.String(http.StatusOK, message)
+	// })
 
 	// For each matched request Context will hold the route definition
 	// router.POST("/user/:name/*action", func(c *gin.Context) {
