28fec04297d38ccf80f695dc3b5c69999ee8a609
yoshi0202 <kobayashi020202@gmail.com>
2020-09-19 11:21:26 +0900

    feat: add constant file

--- app/src/constant/const.go
+++ app/src/constant/const.go
@@ -1,9 +1,13 @@
 package constant
 
 type Constant struct {
-	SvgTag  string
-	RectTag string
-	Url     string
+	SvgTag     string
+	RectTag    string
+	Url        string
+	DbUser     string
+	DbPassword string
+	DbProtocol string
+	DbName     string
 }
 
 func NewConst() *Constant {
@@ -11,6 +15,10 @@ func NewConst() *Constant {
 	c.SvgTag = `<svg(?: [\s\S]+?)?>[\s\S]*?<\/svg>`
 	c.RectTag = `<rect.*?\/>`
 	c.Url = "https://github.com/users/"
+	c.DbUser = "root"
+	c.DbPassword = "password"
+	c.DbProtocol = "tcp(db:3306)"
+	c.DbName = "grass_app"
 
 	return c
 }
