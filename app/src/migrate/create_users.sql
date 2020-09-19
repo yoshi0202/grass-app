4a715a3eda49104132ba978df1508c4b0699d478
yoshi0202 <kobayashi020202@gmail.com>
2020-09-19 11:22:19 +0900

    feat: add migration file

---
+++ app/src/migrate/create_users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  `deleted_at` datetime(3) DEFAULT NULL,
+  `name` longtext,
+  PRIMARY KEY (`id`),
+  KEY `idx_users_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci 
