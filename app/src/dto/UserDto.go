7c95a795cc21da3aa4ad3aaa0b381ad3c3be1ed8
yoshi0202 <kobayashi020202@gmail.com>
2020-09-21 18:50:48 +0900

    feat: create UserDto

--- app/src/dto/UserDto.go
+++ app/src/dto/UserDto.go
@@ -1,11 +1,33 @@
 package dto
 
 import (
+	"encoding/json"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Id   uint
-	Name string
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
+type Users []User
+
+func NewUser() *User {
+	return &User{}
+}
+
+func NewUsers() *Users {
+	return &Users{}
+}
+
+func (u *User) ToJSON() string {
+	json, _ := json.Marshal(u)
+	return string(json)
+}
+
+func (u *Users) ToJSON() string {
+	json, _ := json.Marshal(u)
+	return string(json)
 }
