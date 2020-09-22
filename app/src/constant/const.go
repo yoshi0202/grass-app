f1b799291c9ceaaef48a04aedab55e97b7e548e9
yoshi0202 <kobayashi020202@gmail.com>
2020-09-23 02:06:04 +0900

    feat: change constant env

--- app/src/constant/const.go
+++ app/src/constant/const.go
@@ -1,5 +1,12 @@
 package constant
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Constant struct {
 	SvgTag         string
 	RectTag        string
@@ -16,6 +23,7 @@ type Constant struct {
 }
 
 func NewConst() *Constant {
+	godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
 	c := &Constant{}
 	c.SvgTag = `<svg(?: [\s\S]+?)?>[\s\S]*?<\/svg>`
 	c.RectTag = `<rect.*?\/>`
@@ -24,10 +32,10 @@ func NewConst() *Constant {
 	c.DbPassword = "password"
 	c.DbProtocol = "tcp(db:3306)"
 	c.DbName = "grass_app"
-	c.AccessTokenURL = "https://github.com/login/oauth/access_token"
-	c.GetUserAPIURL = "https://api.github.com/user"
-	c.ClientID = "6275811b400983aaba52"
-	c.ClientSecret = "a9a4d5932feb8b0261912e481ab862a979922a2a"
-	c.RedirectURI = "http://localhost:8080/github/callback"
+	c.AccessTokenURL = os.Getenv("ACCESS_TOKEN_URL")
+	c.GetUserAPIURL = os.Getenv("GET_USER_API_URL")
+	c.ClientID = os.Getenv("CLIENT_ID")
+	c.ClientSecret = os.Getenv("CLIENT_SECRET")
+	c.RedirectURI = os.Getenv("REDIRECT_URI")
 	return c
 }
