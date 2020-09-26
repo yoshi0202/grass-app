6ee5f8f4538894a32019bfa62997676e3f358ace
yoshi0202 <kobayashi020202@gmail.com>
2020-09-26 10:41:32 +0900

    style: grass components

--- front/grass-app/components/Grass.vue
+++ front/grass-app/components/Grass.vue
@@ -1,21 +1,30 @@
 <template>
-  <div>
-    <v-avatar>
-      <img
-        :src="'https://github.com/' + data.githubId + '.png'"
-        :alt="data.githubId"
-      />
-    </v-avatar>
-    <a :href="'https://github.com/' + data.githubId" target="_blank">
-      {{ data.githubId }}
-    </a>
-    <v-btn v-if="!isOwn" text small @click="destroy(data.id)" outlined
-      >delete</v-btn
-    >
+  <v-container text-center>
+    <v-row>
+      <v-col cols="6" offset="3">
+        <v-container my-0 py-0 d-flex justify-space-around>
+          <v-container text-left>
+            <v-avatar>
+              <img
+                :src="'https://github.com/' + data.githubId + '.png'"
+                :alt="data.githubId"
+              />
+            </v-avatar>
+            <a :href="'https://github.com/' + data.githubId" target="_blank">
+              {{ data.githubId }}
+            </a>
+          </v-container>
+          <v-container v-if="!isOwn" text-right>
+            <v-btn ext small @click="destroy(data.id)" outlined>delete</v-btn>
+          </v-container>
+          <v-container v-else> </v-container>
+        </v-container>
+      </v-col>
+    </v-row>
     <v-container text-center>
       <github-lawn :data="data.count" :unit="'contributions'"></github-lawn>
     </v-container>
-  </div>
+  </v-container>
 </template>
 
 <script>
