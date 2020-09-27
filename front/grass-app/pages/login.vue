f787f51c4d37fed7af0d380a7b4274f2cbd185e6
yoshi0202 <kobayashi020202@gmail.com>
2020-09-27 19:12:02 +0900

    style: login pages

--- front/grass-app/pages/login.vue
+++ front/grass-app/pages/login.vue
@@ -4,11 +4,30 @@
     <v-container text-center>
       <v-row>
         <v-col sm="4" offset-sm="4">
-          <v-card
-            tile
-            href="https://github.com/login/oauth/authorize?login=grass-app&client_id=6275811b400983aaba52&redirect_uri=http://localhost:8080/github/callback"
-            >Signed in GitHub</v-card
-          >
+          <v-container>
+            <v-card flat tile outlined>
+              <v-container>
+                <v-container class="text-h5">
+                  SignIn With GitHub
+                  <v-container mb-6>
+                    <v-layout justify-center>
+                      <v-img
+                        lazy-src="/GitHub-Mark-120px-plus.png"
+                        src="/GitHub-Mark-120px-plus.png"
+                        max-height="100"
+                        max-width="100"
+                      ></v-img>
+                    </v-layout>
+                  </v-container>
+                  <v-btn
+                    outlined
+                    href="https://github.com/login/oauth/authorize?login=grass-app&client_id=6275811b400983aaba52&redirect_uri=http://localhost:8080/github/callback"
+                    >SignIN</v-btn
+                  >
+                </v-container>
+              </v-container>
+            </v-card>
+          </v-container>
         </v-col>
       </v-row>
     </v-container>
