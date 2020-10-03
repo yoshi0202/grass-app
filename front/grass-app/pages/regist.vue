38162ddf7b5135bb27013fa94886c6b5600127d2
yoshi0202 <kobayashi020202@gmail.com>
2020-10-03 13:44:18 +0900

    feat: create mypages

--- front/grass-app/pages/regist.vue
+++ front/grass-app/pages/regist.vue
@@ -1,24 +1,51 @@
 <template>
-  <v-container>
-    <v-card flat tile class="text-h1 d-flex justify-center">Register</v-card>
-    <v-row>
-      <v-col md="4" offset-md="4">
-        <v-container text-center>
-          <v-card tile>
-            input GitHubID
-            <v-container>
-              <v-text-field
-                label="GitHubID"
-                single-line
-                outlined
-                v-model="gitHubID"
-              ></v-text-field>
-              <v-btn text small @click="regist()" outlined>Register</v-btn>
+  <v-container text-center>
+    <v-card flat tile class="text-h1 d-flex justify-center">YourPages</v-card>
+    <v-container>
+      <v-row>
+        <v-col md="4" offset-md="4">
+          <v-container m-0 p-0 d-flex justify-space-around>
+            <v-avatar class="mr-5" size="100">
+              <img
+                :src="'https://github.com/' + ownData.login + '.png'"
+                :alt="ownData.login"
+              />
+            </v-avatar>
+            <v-container m-0 p-0>
+              <p>GitHubID: {{ ownData.login }}</p>
+              <p>
+                YourPage:
+                <a :href="'https://github.com/' + ownData.login"
+                  >https://github.com/{{ ownData.login }}</a
+                >
+              </p>
             </v-container>
-          </v-card>
-        </v-container>
-      </v-col>
-    </v-row>
+          </v-container>
+          <v-container>
+            <v-card flat tile outlined>
+              <v-container>
+                <v-container class="text-h5">
+                  GitHubIDを入力
+                  <v-container>
+                    <v-container class="text-subtitle-1">
+                      <p>入力されたGitHubユーザの草が</p>
+                      <p>Timelineに表示されるようになります。</p>
+                    </v-container>
+                    <v-text-field
+                      label="GitHubID"
+                      single-line
+                      outlined
+                      v-model="gitHubID"
+                    ></v-text-field>
+                    <v-btn @click="regist()" outlined>Register</v-btn>
+                  </v-container>
+                </v-container>
+              </v-container>
+            </v-card>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
@@ -28,9 +55,12 @@ export default {
     return {
       ip: "aiueo",
       gitHubID: "",
+      ownData: "aiueo",
     };
   },
-  mounted: async function () {},
+  mounted: async function () {
+    this.getOwn();
+  },
   methods: {
     async regist() {
       console.log(this.gitHubID);
@@ -49,6 +79,16 @@ export default {
       }
       if (registResult === "success!") {
         this.$router.push("/timeline");
+        return;
+      }
+    },
+    async getOwn() {
+      const ownData = await this.$axios.$get("http://localhost:8080/user", {
+        withCredentials: true,
+      });
+      this.ownData = ownData;
+      if (ownData === "redirect") {
+        this.$router.push("/login");
         return;
       }
     },
